278f06920bfbdd3c361eaf4b4dc272192418788c
christopher morris <slurp@v3107-0ac543a6.wifi.cf.ac.uk>
2020-06-30 11:45:53 +0100

    have basic list in DOM

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+// model/data code
 class PeopleModel {
-
     constructor(){
         this.people = [
             {name: "alice", age: 20},
@@ -31,9 +31,55 @@ class PeopleModel {
         })
     }
 
+    get getPeople(){
+        return this.people;
+    }
+
+    addPerson(name, age){
+        // implement
+    }
 }
 
 let peopleModel = new PeopleModel();
+// test model calculations
 console.log(peopleModel.averageAge);
 console.log(peopleModel.oldestPerson);
-console.log(peopleModel.youngestPerson)
+console.log(peopleModel.youngestPerson);
+
+// view code
+
+var chatContainerRef = document.getElementById("people-container");
+
+function render(){
+    peopleModel.getPeople.forEach((person) => {
+        addPersonToDOM(person.name, person.age);
+    });
+}
+
+function addPersonToDOM(name, age){
+    var personBoxNode = document.createElement("div");
+    personBoxNode.id = "people-box";
+
+    var nameContainer = document.createElement("span");
+    var nameCommentNode = document.createTextNode("name: " + name);
+    nameContainer.appendChild(nameCommentNode);
+    nameContainer.id = "person-name";
+
+    var ageContainer = document.createElement("span");
+    var ageCommentNode = document.createTextNode("age: " + age);
+    ageContainer.appendChild(ageCommentNode);
+    ageContainer.id = "person-age";
+
+    var clearDiv = document.createElement("div");
+    clearDiv.id = "clear";
+
+    personBoxNode.appendChild(nameContainer);
+    personBoxNode.appendChild(ageContainer);
+    personBoxNode.appendChild(clearDiv);
+
+
+
+    chatContainerRef.appendChild(personBoxNode);
+}
+
+render();
